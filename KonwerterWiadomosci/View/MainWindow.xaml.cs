35166dd06628abb76a6249ae2618acce79072d2d
Endrju626 <114163598+Endrju626@users.noreply.github.com>
2023-06-23 15:15:02 +0200

    c#

---
+++ KonwerterWiadomosci/View/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using KonwerterWiadomosci.ViewModel;
+using Twilio;
+namespace KonwerterWiadomosci
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            TextMessage.Text = string.Empty;
+           
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            string txt = TextMessage.Text;
+           // string nadawca = WhoSend.Text;
+            string number = PhoneNumber.Text;
+
+            //APISMS.SendSms(message, nadawca, number);
+            Twiloo.TwilooSms(txt, number);
+        }
+
+        private void ListBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            TextMessage.Text = " Zapraszam jutro do pracy na 13";
+            PhoneNumber.Text = "03941";
+        }
+
+        private void ListBoxItem_Selected_1(object sender, RoutedEventArgs e)
+        {
+            TextMessage.Text = " Jutro wolne.";
+            PhoneNumber.Text = "2311211";
+             
+        }
+
+        private void ListBoxItem_Selected_2(object sender, RoutedEventArgs e)
+        {
+            TextMessage.Text = "Proszę wejść tylnym wyjściem.";
+            PhoneNumber.Text = " 799";
+            
+        }
+    }
+}
---
+++ KonwerterWiadomosci/ViewModel/Twiloo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace KonwerterWiadomosci.ViewModel
+{
+    internal class Twiloo
+    {
+        internal static void TwilooSms(string txt, string number)
+        {
+            TwilioClient.Init("AC67be2dc30f3f4f0eee98d564629c549d", "fde153d2858af7085d0d577284acae6e");
+
+            var message = MessageResource.Create(
+                new PhoneNumber(number),
+                from: new PhoneNumber("+13159155600"),
+                body: txt
+            );
+            Console.WriteLine(message.Sid);
+            MessageBox.Show("Wiadomość SMS została wysłana pomyślnie.", "Sukces", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
+
+
+    
